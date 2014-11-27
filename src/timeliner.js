a7fa5bb6f2bea6642385f6067adc1d314dbe7abe
Joshua Koo <zz85nus@gmail.com>
2014-11-27 10:09:21 +0800

    Split undo and dispatcher into components

--- src/timeliner.js
+++ src/timeliner.js
@@ -2,7 +2,11 @@
  * @author Joshua Koo http://joshuakoo.com
  */
 
-(function() {
+var undo = require('./undo'),
+	Dispatcher = require('./dispatcher'),
+	UndoManager = undo.UndoManager,
+	UndoState = undo.UndoState
+	;
 
 	var LINE_HEIGHT = 20;
 	var DIAMOND_SIZE = 10;
@@ -759,104 +763,7 @@
 
 	}
 
-	/**************************/
-	// Undo Manager
-	/**************************/
-
-	function UndoState(state, description) {
-		this.state = JSON.stringify(state);
-		this.description = description;
-		console.log('Saved: ', description);
-	}
-	 
-	function UndoManager(max) {
-		this.MAX_ITEMS = max || 100;
-		this.clear();
-	}
-	 
-	UndoManager.prototype.save = function(state) {
-		var states = this.states;
-		var next_index = this.index + 1;
-		var to_remove = states.length - next_index;
-		states.splice(next_index, to_remove, state);
-	 
-		if (states.length > this.MAX_ITEMS) {
-			states.shift();
-		}
-	 
-		this.index = states.length - 1;
-	};
-	 
-	UndoManager.prototype.clear = function() {
-		this.states = [];
-		this.index = -1;
-		// FIXME: leave default state or always leave one state?
-	};
-	 
-	UndoManager.prototype.canUndo = function() {
-		return this.index > 0;
-	};
-	 
-	UndoManager.prototype.canRedo = function() {
-		return this.index < this.states.length - 1;
-	};
-	 
-	UndoManager.prototype.undo = function() {
-		if (this.canUndo()) {
-			this.index--;
-		}
-	 
-		return this.get();
-	};
-	 
-	 
-	UndoManager.prototype.redo = function() {
-		if (this.canRedo()) {
-			this.index++;
-		}
-	 
-		return this.get();
-	};
-	 
-	UndoManager.prototype.get = function() {
-		return this.states[this.index];
-	};
-
-
-	/**************************/
-	// Dispatcher
-	/**************************/
-
-	function Dispatcher() {
-
-		var event_listeners = {
-
-		};
-
-		function on(type, listener) {
-			if (!(type in event_listeners)) {
-				event_listeners[type] = [];
-			}
-			var listeners = event_listeners[type];
-			listeners.push(listener);
-		}
-
-		function fire(type) {
-			var args = Array.prototype.slice.call(arguments);
-			args.shift();
-			var listeners = event_listeners[type];
-			if (!listeners) return;
-			for (var i = 0; i < listeners.length; i++) {
-				var listener = listeners[i];
-				listener.apply(listener, args);
-			}
-		}
-
-		this.on = on;
-		this.fire = fire;
-
-	}
-
+	
 	function findTimeinLayer(layer, time) {
 		var values = layer.values;
 		var i, il;
@@ -1197,4 +1104,3 @@
 	}
 
 	window.Timeliner = TimelineController;
-})();
---
+++ src/undo.js
@@ -0,0 +1,66 @@
+/**************************/
+// Undo Manager
+/**************************/
+
+function UndoState(state, description) {
+	this.state = JSON.stringify(state);
+	this.description = description;
+	console.log('Saved: ', description);
+}
+
+function UndoManager(max) {
+	this.MAX_ITEMS = max || 100;
+	this.clear();
+}
+
+UndoManager.prototype.save = function(state) {
+	var states = this.states;
+	var next_index = this.index + 1;
+	var to_remove = states.length - next_index;
+	states.splice(next_index, to_remove, state);
+
+	if (states.length > this.MAX_ITEMS) {
+		states.shift();
+	}
+
+	this.index = states.length - 1;
+};
+
+UndoManager.prototype.clear = function() {
+	this.states = [];
+	this.index = -1;
+	// FIXME: leave default state or always leave one state?
+};
+
+UndoManager.prototype.canUndo = function() {
+	return this.index > 0;
+};
+
+UndoManager.prototype.canRedo = function() {
+	return this.index < this.states.length - 1;
+};
+
+UndoManager.prototype.undo = function() {
+	if (this.canUndo()) {
+		this.index--;
+	}
+
+	return this.get();
+};
+
+UndoManager.prototype.redo = function() {
+	if (this.canRedo()) {
+		this.index++;
+	}
+
+	return this.get();
+};
+
+UndoManager.prototype.get = function() {
+	return this.states[this.index];
+};
+
+module.exports = {
+	UndoState: UndoState,
+	UndoManager: UndoManager
+};
