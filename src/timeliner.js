3f9a51822773d19c3af57517b1b3626335b2e7f6
Joshua Koo <zz85nus@gmail.com>
2014-12-06 12:46:27 +0800

    Quick and dirty hack for Movable+Snappable widget

--- src/timeliner.js
+++ src/timeliner.js
@@ -27,15 +27,30 @@ function LayerProp(name) {
 	*/
 }
 
+function Data() {
+	this.version = 1.1;
+	this.modified = new Date().toString();
+	this.title = 'Untitled';
+
+	this.layers = [];
+}
+
 function Timeliner(target) {
 	// Aka Layer Manager / Controller
 
 	// Should persist current time too.
 	var layers = [];
 	window.l2 = layers;
-	var div = document.createElement('div');
 
+	var div = document.createElement('div');
 	div.style.backgroundColor = Theme.a;
+
+	var pane = document.createElement('div');
+	pane.id = 'pane';
+	pane.appendChild(div);
+
+	var ghostpane = document.createElement('div');
+	ghostpane.id = 'ghostpane';
 
 	var dispatcher = new Dispatcher();
 
@@ -274,7 +289,7 @@ function Timeliner(target) {
 		}
 	};
 
-	div.style.cssText = 'position: absolute; top: 0px;'; // resize: both; left: 50px;
+	div.style.cssText = 'position: absolute; top: 10px;'; // resize: both; left: 50px;
 
 	div.appendChild(layer_panel.dom);
 	div.appendChild(timeline.dom);
@@ -317,14 +332,17 @@ function Timeliner(target) {
 		}
 	});
 
+	// window.addEventListener('resize', function() {
+	// 	resize(innerWidth, innerHeight);
+	// });
 
-
-	window.addEventListener('resize', function() {
-		console.log('resized', innerWidth, innerHeight);
-		Settings.width = innerWidth - Settings.LEFT_PANE_WIDTH;
+	function resize(width, height) {
+		console.log('resized', width, height);
+		Settings.width = width - Settings.LEFT_PANE_WIDTH;
+		Settings.height = height;
 		timeline.resize();
 		timeline.repaint();
-	});
+	}
 
 	function restyle(left, right) {
 		left.style.cssText = 'position: absolute; left: 0px; top: 0px; height: ' + Settings.height + 'px;background: ' + Theme.a + ';';
@@ -337,7 +355,9 @@ function Timeliner(target) {
 	}
 
 
-	document.body.appendChild(div);
+	// document.body.appendChild(div);
+	document.body.appendChild(pane);
+	document.body.appendChild(ghostpane);
 
 	function addLayer(name) {
 		var layer = new LayerProp(name);
@@ -350,6 +370,278 @@ function Timeliner(target) {
 	}
 
 	this.addLayer = addLayer;
+
+	(function() {
+		"use strict";
+
+		// Minimum resizable area
+		var minWidth = 100;
+		var minHeight = 40;
+
+		// Thresholds
+		var FULLSCREEN_MARGINS = -10;
+		var MARGINS = 4;
+
+		// End of what's configurable.
+		var clicked = null;
+		var onRightEdge, onBottomEdge, onLeftEdge, onTopEdge;
+
+		var rightScreenEdge, bottomScreenEdge;
+
+		var preSnapped;
+
+		var b, x, y;
+
+		var redraw = false;
+
+		var pane = document.getElementById('pane');
+		var ghostpane = document.getElementById('ghostpane');
+
+		function setBounds(element, x, y, w, h) {
+			element.style.left = x + 'px';
+			element.style.top = y + 'px';
+			element.style.width = w + 'px';
+			element.style.height = h + 'px';
+
+			if (element === pane) {
+				console.log('presss')
+				resize(w, h);
+			}
+		}
+
+		function hintHide() {
+			setBounds(ghostpane, b.left, b.top, b.width, b.height);
+			ghostpane.style.opacity = 0;
+
+			// var b = ghostpane.getBoundingClientRect();
+			// ghostpane.style.top = b.top + b.height / 2;
+			// ghostpane.style.left = b.left + b.width / 2;
+			// ghostpane.style.width = 0;
+			// ghostpane.style.height = 0;
+		}
+
+
+		// Mouse events
+		pane.addEventListener('mousedown', onMouseDown);
+		document.addEventListener('mousemove', onMove);
+		document.addEventListener('mouseup', onUp);
+
+		// Touch events	
+		pane.addEventListener('touchstart', onTouchDown);
+		document.addEventListener('touchmove', onTouchMove);
+		document.addEventListener('touchend', onTouchEnd);
+
+
+		function onTouchDown(e) {
+			onDown(e.touches[0]);
+			e.preventDefault();
+		}
+
+		function onTouchMove(e) {
+			onMove(e.touches[0]);		
+		}
+
+		function onTouchEnd(e) {
+			if (e.touches.length ==0) onUp(e.changedTouches[0]);
+		}
+
+		function onMouseDown(e) {
+			onDown(e);
+			e.preventDefault();
+		}
+
+		function onDown(e) {
+			calc(e);
+
+			var isResizing = onRightEdge || onBottomEdge || onTopEdge || onLeftEdge;
+
+			clicked = {
+				x: x,
+				y: y,
+				cx: e.clientX,
+				cy: e.clientY,
+				w: b.width,
+				h: b.height,
+				isResizing: isResizing,
+				isMoving: !isResizing && canMove(),
+				onTopEdge: onTopEdge,
+				onLeftEdge: onLeftEdge,
+				onRightEdge: onRightEdge,
+				onBottomEdge: onBottomEdge
+			};
+		}
+
+		function canMove() {
+			return x > 0 && x < b.width && y > 0 && y < b.height
+			&& y < 30;
+		}
+
+		function calc(e) {
+			b = pane.getBoundingClientRect();
+			x = e.clientX - b.left;
+			y = e.clientY - b.top;
+
+			onTopEdge = y < MARGINS;
+			onLeftEdge = x < MARGINS;
+			onRightEdge = x >= b.width - MARGINS;
+			onBottomEdge = y >= b.height - MARGINS;
+
+			rightScreenEdge = window.innerWidth - MARGINS;
+			bottomScreenEdge = window.innerHeight - MARGINS;
+		}
+
+		var e;
+
+		function onMove(ee) {
+			calc(ee);
+
+			e = ee;
+
+			redraw = true;
+
+		}
+
+		function animate() {
+
+			requestAnimationFrame(animate);
+
+			if (!redraw) return;
+
+			redraw = false;
+
+			if (clicked && clicked.isResizing) {
+
+				if (clicked.onRightEdge) pane.style.width = Math.max(x, minWidth) + 'px';
+				if (clicked.onBottomEdge) pane.style.height = Math.max(y, minHeight) + 'px';
+
+				if (clicked.onLeftEdge) {
+					var currentWidth = Math.max(clicked.cx - e.clientX  + clicked.w, minWidth);
+					if (currentWidth > minWidth) {
+						pane.style.width = currentWidth + 'px';
+						pane.style.left = e.clientX + 'px';	
+					}
+				}
+
+				if (clicked.onTopEdge) {
+					var currentHeight = Math.max(clicked.cy - e.clientY  + clicked.h, minHeight);
+					if (currentHeight > minHeight) {
+						pane.style.height = currentHeight + 'px';
+						pane.style.top = e.clientY + 'px';	
+					}
+				}
+
+				hintHide();
+
+				resize(b.width, b.height);
+
+				return;
+			}
+
+			if (clicked && clicked.isMoving) {
+
+				if (b.top < FULLSCREEN_MARGINS || b.left < FULLSCREEN_MARGINS || b.right > window.innerWidth - FULLSCREEN_MARGINS || b.bottom > window.innerHeight - FULLSCREEN_MARGINS) {
+					// hintFull();
+					setBounds(ghostpane, 0, 0, window.innerWidth, window.innerHeight);
+					ghostpane.style.opacity = 0.2;
+				} else if (b.top < MARGINS) {
+					// hintTop();
+					setBounds(ghostpane, 0, 0, window.innerWidth, window.innerHeight / 2);
+					ghostpane.style.opacity = 0.2;
+				} else if (b.left < MARGINS) {
+					// hintLeft();
+					setBounds(ghostpane, 0, 0, window.innerWidth / 2, window.innerHeight);
+					ghostpane.style.opacity = 0.2;
+				} else if (b.right > rightScreenEdge) {
+					// hintRight();
+					setBounds(ghostpane, window.innerWidth / 2, 0, window.innerWidth / 2, window.innerHeight);
+					ghostpane.style.opacity = 0.2;
+				} else if (b.bottom > bottomScreenEdge) {
+					// hintBottom();
+					setBounds(ghostpane, 0, window.innerHeight / 2, window.innerWidth, window.innerWidth / 2);
+					ghostpane.style.opacity = 0.2;
+				} else {
+					hintHide();
+				}
+
+				if (preSnapped) {
+					setBounds(pane,
+						e.clientX - preSnapped.width / 2,
+						e.clientY - Math.min(clicked.y, preSnapped.height),
+						preSnapped.width,
+						preSnapped.height
+					);
+					return;
+				}
+
+				// moving
+				pane.style.top = (e.clientY - clicked.y) + 'px';
+				pane.style.left = (e.clientX - clicked.x) + 'px';
+
+				return;
+			}
+
+			// This code executes when mouse moves without clicking
+
+			// style cursor
+			if (onRightEdge && onBottomEdge || onLeftEdge && onTopEdge) {
+				pane.style.cursor = 'nwse-resize';
+			} else if (onRightEdge && onTopEdge || onBottomEdge && onLeftEdge) {
+				pane.style.cursor = 'nesw-resize';
+			} else if (onRightEdge || onLeftEdge) {
+				pane.style.cursor = 'ew-resize';
+			} else if (onBottomEdge || onTopEdge) {
+				pane.style.cursor = 'ns-resize';
+			} else if (canMove()) {
+				pane.style.cursor = 'move';
+			} else {
+				pane.style.cursor = 'default';
+			}
+		}
+
+		animate();
+
+		function onUp(e) {
+			calc(e);
+
+			if (clicked && clicked.isMoving) {
+				// Snap
+				var snapped = {
+					width: b.width,
+					height: b.height
+				};
+
+				if (b.top < FULLSCREEN_MARGINS || b.left < FULLSCREEN_MARGINS || b.right > window.innerWidth - FULLSCREEN_MARGINS || b.bottom > window.innerHeight - FULLSCREEN_MARGINS) {
+					// hintFull();
+					setBounds(pane, 0, 0, window.innerWidth, window.innerHeight);
+					preSnapped = snapped;
+				} else if (b.top < MARGINS) {
+					// hintTop();
+					setBounds(pane, 0, 0, window.innerWidth, window.innerHeight / 2);
+					preSnapped = snapped;
+				} else if (b.left < MARGINS) {
+					// hintLeft();
+					setBounds(pane, 0, 0, window.innerWidth / 2, window.innerHeight);
+					preSnapped = snapped;
+				} else if (b.right > rightScreenEdge) {
+					// hintRight();
+					setBounds(pane, window.innerWidth / 2, 0, window.innerWidth / 2, window.innerHeight);
+					preSnapped = snapped;
+				} else if (b.bottom > bottomScreenEdge) {
+					// hintBottom();
+					setBounds(pane, 0, window.innerHeight / 2, window.innerWidth, window.innerWidth / 2);
+					preSnapped = snapped;
+				} else {
+					preSnapped = null;
+				}
+
+				hintHide();
+
+			}
+
+			clicked = null;
+
+		}
+	})();
 
 }
 
