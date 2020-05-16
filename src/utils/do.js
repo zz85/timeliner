f2e5c6f259ddf4fb04779df795897c50e0863f19
Joshua Koo <zz85nus@gmail.com>
2020-05-16 10:21:50 -0700

    Import do.js

---
+++ src/utils/do.js
@@ -0,0 +1,16 @@
+function Do(parent) {
+	var listeners = [];
+	this.do = function(callback) {
+		listeners.push(callback);
+	};
+	this.undo = function(callback) {
+		listeners.splice(listeners.indexOf(callback), 1);
+	};
+	this.fire = function() {
+		for (var v = 0; v < listeners.length; v++) {
+			listeners[v].apply(parent, arguments);
+		}
+	};
+}
+
+export { Do }
