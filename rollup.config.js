566227d3337649d9ce2607fcb4bc757f9de0fcde
Shauna Zhang <shengchenzhang1207@gmail.com>
2021-06-16 10:47:09 +0800

    Make compatible with NPM install

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
-simport { terser } from "rollup-plugin-terser";
+import { terser } from "rollup-plugin-terser";
 
 export default {
 	input: 'src/timeliner.js',
 	output: [
-		{ file: "build/lib.js", format: "cjs" },
+		{ file: "build/index.js", format: "cjs" },
 		{ file: "build/timeliner.min.js", plugins: [terser()] },
-		{ file: "build/timeliner.js", format: "esm" }
+		{ file: "build/index.es.js", format: "esm" }
 	]
 };
